8ed08d7b1fb97569bda77a0e24ac8e3f4fe9a260
chenzhiwei2 <44861417+chenzhiwei2@users.noreply.github.com>
2018-11-15 17:48:13 +0800

    版本升级

--- app/src/main/java/com/sitong/changqin/ui/fragment/TunerFragment.kt
+++ app/src/main/java/com/sitong/changqin/ui/fragment/TunerFragment.kt
@@ -1,6 +1,9 @@
 package com.sitong.changqin.ui.fragment
 
 import android.view.View
+import be.tarsos.dsp.io.android.AudioDispatcherFactory
+import be.tarsos.dsp.pitch.PitchDetectionHandler
+import be.tarsos.dsp.pitch.PitchProcessor
 import com.jyall.bbzf.base.BaseFragment
 import com.jyall.bbzf.base.BasePresenter
 import com.jyall.bbzf.base.IBaseView
@@ -12,6 +15,7 @@ import kotlinx.android.synthetic.main.fragment_tuner.*
  * create by chen.zhiwei on 2018-8-15
  */
 class TunerFragment : BaseFragment<IBaseView, BasePresenter<IBaseView>>(), IBaseView {
+    internal var mThread: Thread? = null
     override fun getLayoutId(): Int = R.layout.fragment_tuner
     private var list_1 = arrayListOf<String>("紧五弦", "紧二五弦", "正调")
     private var list_2 = arrayListOf<String>("1", "2", "3", "4", "5", "6", "7")
@@ -25,6 +29,19 @@ class TunerFragment : BaseFragment<IBaseView, BasePresenter<IBaseView>>(), IBase
 
         picker_01.data = list_1 as List<CharSequence>?
         picker_02.data = list_2 as List<CharSequence>?
+
+
+        val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0)
+
+        val pdh = PitchDetectionHandler { result, e ->
+            val pitchInHz = result.pitch
+//            runOnUiThread(Runnable { tv_pinlv.setText("" + pitchInHz) })
+        }
+        val p = PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050f, 1024, pdh)
+        dispatcher.addAudioProcessor(p)
+        mThread = Thread(dispatcher, "Audio Dispatcher")
+//        mThread?.start()
+
     }
 
     override fun isRegistEventBus(): Boolean = false
