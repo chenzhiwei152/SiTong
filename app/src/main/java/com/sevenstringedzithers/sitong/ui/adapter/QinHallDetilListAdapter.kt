f33a0041c670f8d13ac0530dcea8707dc6000ee8
chenzhiwei2 <44861417+chenzhiwei2@users.noreply.github.com>
2018-12-03 11:56:51 +0800

    动画

--- app/src/main/java/com/sevenstringedzithers/sitong/ui/adapter/QinHallDetilListAdapter.kt
+++ app/src/main/java/com/sevenstringedzithers/sitong/ui/adapter/QinHallDetilListAdapter.kt
@@ -1,13 +1,18 @@
 package com.sevenstringedzithers.sitong.ui.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jyall.bbzf.extension.loadImage
 import com.sevenstringedzithers.sitong.R
+import com.sevenstringedzithers.sitong.base.Constants
 import com.sevenstringedzithers.sitong.mvp.model.bean.QinguanDetailBean
+import com.sevenstringedzithers.sitong.ui.activity.VideoPlayActivity
 import com.sevenstringedzithers.sitong.ui.listerner.RVAdapterItemOnClick
+import kotlinx.android.synthetic.main.item_qin_hanll_detail.view.*
 
 /**
  * create by chen.zhiwei on 2018-8-14
@@ -39,8 +44,14 @@ class QinHallDetilListAdapter(var context: Context) : RecyclerView.Adapter<QinHa
     override fun getItemCount(): Int = list.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        holder.viewLayout.tv_name.text = "" + list[position].ctn
-//        holder.viewLayout.iv_image.loadImage(context, list[position]?.img)
+        holder.viewLayout.tv_name.text = "" + list[position].ctn
+        holder.viewLayout.iv_image.loadImage(context, list[position]?.img)
+        holder.viewLayout.tv_video.text=""+list[position].video
+        holder.viewLayout.tv_video.setOnClickListener {
+            var intent = Intent(context, VideoPlayActivity::class.java)
+            intent.putExtra(Constants.VIDOE_URL,list[position].video)
+            context.startActivity(intent)
+        }
     }
 
 
